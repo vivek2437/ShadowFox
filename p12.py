1b4905a4592637a13047c4af7b2c191d319c81c7
vivek2437 <nayivivek212@gmail.com>
2025-04-04 23:09:15 +0530

    p12.py

---
+++ p12.py
@@ -0,0 +1,5 @@
+# 2. Create a variable called for and assign it a value 4. See what
+# happens and find out the reason behind the behavior that you
+# see.
+for=4
+#in that case for is the keyword use in for loop and keyword name not use in varables
