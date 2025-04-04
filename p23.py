0a2c344b5a6b8e271f23b41bc0353c8b6c38e4df
vivek2437 <nayivivek212@gmail.com>
2025-04-04 23:26:09 +0530

    p23.py

---
+++ p23.py
@@ -0,0 +1,8 @@
+# 3. If you cross a 490meterlong street in 7 minutes, calculate your
+# speed in meters per second. Print the answer without any decimal
+# point in it. Hint: Speed = Distance / Time
+distance=490
+t1=7
+t=t1*60
+speed=int(distance/t)
+print("SPEED= ",speed,"meter per second")
