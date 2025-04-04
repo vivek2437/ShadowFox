df581bec45db6083b4044b457c9717a45c61fd63
vivek2437 <nayivivek212@gmail.com>
2025-04-04 23:17:13 +0530

    p21.py

---
+++ p21.py
@@ -0,0 +1,9 @@
+# Write a function that takes two arguments, 145 and 'o'
+# , and
+# uses the `format` function to return a formatted string. Print the
+# result. Try to identify the representation used.
+def(num,fmt):
+return format(num,fmt)
+
+r = format_number(145, 'o')
+print("Formatted result:", r)
