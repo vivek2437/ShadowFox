15eb2d2f2c657dd107d2cc01c2179bb5f25a48de
vivek2437 <nayivivek212@gmail.com>
2025-04-04 23:11:44 +0530

    p13.py

---
+++ p13.py
@@ -0,0 +1,8 @@
+# 3. Store the principal amount, rate of interest, and time in
+# different variables and then calculate the Simple Interest for 3
+# years.
+principal_amount=34000
+rate_of_interest=4
+time=3
+interest=(principal_amount*rate_of_interest*time)/100
+print(interest)
